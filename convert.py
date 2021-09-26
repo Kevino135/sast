64dc436b800ce89e9d050baf3acdc6afe9abde76
Kevino135 <89955980+Kevino135@users.noreply.github.com>
2021-09-26 21:56:39 +0700

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,55 +1,72 @@
 #!/usr/bin/env python3
 
 import json
-import os
 
 class bstyles:
-	NO_EFFECT = "\033[0;" 
-	BOLD = "\033[1;"
-	UNDERLINE = "\033[2;"
-	NEGATIVE1 = "\033[3;"
-	NEGATIVE2 = '\033[5;'
+        NO_EFFECT = "\033[0;" 
+        BOLD = "\033[1;"
+        UNDERLINE = "\033[2;"
+        NEGATIVE1 = "\033[3;"
+        NEGATIVE2 = '\033[5;'
 
 class bcolors:
-	BLACK = "30m"
-	RED = "31m"
-	GREEN = "32m"
-	YELLOW = "33m"
-	BLUE = "34m"
-	PURPLE = "35m"
-	CYAN = "36m"
-	WHITE = "37m"
-	ENDC = "0m"
+        BLACK = "30m"
+        RED = "31m"
+        GREEN = "32m"
+        YELLOW = "33m"
+        BLUE = "34m"
+        PURPLE = "35m"
+        CYAN = "36m"
+        WHITE = "37m"
+        ENDC = "0m"
+
+index = 1
+def print_report(data):
+        global index
+
+        important_key = ['reason', 'stringsfound', 'text', 'author']
+
+        for all_result in data:
+                print("{}Issue \#{}{}".format(issue_color, index, endc))
+                for key, value in all_result.items():
+                        if key.lower() in important_key:
+                                print(f"{result_key}{key.capitalize() : <30}{endc}: {custom_result_value}{value}{endc}")
+                        else:
+                                print(f"{result_key}{key.capitalize() : <30}{endc}: {value}")
+                index += 1
+                print("\n")
+
 
 def headers():
-	print("+", "-"*46, "+", sep="")
-	print("|", " "*20, "REPORT", " "*20, "|", sep="")
-	print("+", "-"*46, "+", sep="")
+        print("+", "-"*46, "+", sep="")
+        print("|", " "*20, "REPORT", " "*20, "|", sep="")
+        print("+", "-"*46, "+", sep="")
 
+# text color
+scanner_name = bstyles.NO_EFFECT + bcolors.CYAN
+scanner_name_bold = bstyles.BOLD + bcolors.CYAN
+issue_color = bstyles.BOLD + bcolors.RED
+result_key = bstyles.BOLD + bcolors.PURPLE
+custom_result_value = bstyles.BOLD + bcolors.GREEN
+endc = bstyles.NO_EFFECT + bcolors.ENDC
 def main():
-	scanner_name = bstyles.NO_EFFECT + bcolors.CYAN
-	scanner_name_bold = bstyles.BOLD + bcolors.CYAN
-	issue_color = bstyles.BOLD + bcolors.RED
-	result_key_color = bstyles.BOLD + bcolors.PURPLE
-	endc = bstyles.NO_EFFECT + bcolors.ENDC
+        headers()
+        print("{}>> Scanner ===> {}TruffleHog{}".format(scanner_name, scanner_name_bold, endc))
 
-#	rows, columns = os.popen('stty size', 'r').read().split()
-	headers()
+        openFile = open("tfhog.json", "r")
+        data = json.load(openFile)
+        openFile.close()
 
-	print("{}>> Scanner ===> {}TruffleHog{}".format(scanner_name, scanner_name_bold, endc))
+        print_report(data)
 
-	openFile = open("tfhog.json", "r")
-	data = json.load(openFile)
-	openFile.close()
+        print("{}>> Scanner ===> {}Golangci-lint{}".format(scanner_name, scanner_name_bold, endc))
 
-	index = 1
+        openFile = open("result.json", "r")
+        data = json.load(openFile)
+        data = data["Issues"]
+        openFile.close()
 
-	for all_result in data:
-		print("{}Issue #{}{}".format(issue_color, index, endc))
-		for key, value in all_result.items():
-			print("{}".format(result_key_color), f"{key.capitalize() : <13}{endc}", ": {}".format(value), sep="")
-		index += 1
-		print("\n")
+        print_report(data)
 
 if __name__ == "__main__":
-	main()
+        main()
